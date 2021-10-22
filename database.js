31d936a8c8e369406afa4bca2126c7b3bdf18851
James Zaman <jzaman182040@bscse.uiu.ac.bd>
2021-10-22 22:43:22 +0600

    Connected to database

---
+++ database.js
@@ -0,0 +1,16 @@
+// imports
+const mongoose = require("mongoose");
+require("dotenv").config(); // Getting all the environment variables.
+
+// Database connection
+mongoose.connect(
+  `mongodb+srv://${DATABASE_USER}:${process.env.DATABASE_PASSWORD}@main.adjy4.mongodb.net/botdatabase?retryWrites=true&w=majority`,
+  { useNewUrlParser: true, useUnifiedTopology: true }
+);
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "Connection ERROR :(:"));
+db.once("open", function () {
+  console.log("Database connected");
+});
+
+
