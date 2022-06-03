4ba637ebdf1bfe77080e8b69e597bd97c562b3aa
jameszaman <james.hedayet@gmail.com>
2022-06-03 12:41:33 +0600

    Updated git documentation to match current commands.

--- functions/documentation.js
+++ functions/documentation.js
@@ -4,25 +4,14 @@ let doc = `
 	All commands must start with '!'
 	
 	***List of commands***
-	kick [id]
-		Used to kick any member.
 	ban [id]
 		Used to ban any member.
 	doc
 		Get the documentation for this BOT!
-	whohaspower
-		Says the name of the most omnipotent being in all of existence.
-	joke
-		Says a random joke from the list of jokes in the database.
+	kick [id]
+		Used to kick any member.
 	weight
 		Guesses your weight.
-	*Guess Game Commands*
-		guessgamestart
-			starts the guess game.
-		guessgameend
-			ends the gues game.
-		guess
-			to guess the randomly generated number.
 `
 
 function sendDocumentation(message) {
