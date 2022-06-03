4ee67d726a2934ec3413c2d5ffd38bff8bd845c2
jameszaman <james.hedayet@gmail.com>
2022-06-03 17:17:35 +0600

    Created a function for getting different subreddit infos.

--- functions/reddit.js
+++ functions/reddit.js
@@ -2,12 +2,25 @@ const { spawn } = require('child_process');
 require('dotenv').config();
 
 
-const reddit = spawn('python', [`${__dirname}/reddit.py`, process.env.CLIENT_ID, process.env.CLIENT_SECRET, process.env.USER_AGENT])
+function updateRedditPosts(subreddits) {
+  subreddits.forEach(subreddit => {
+    // Initial arguments are what is necessary for Reddit API.
+    const arguments = [`${__dirname}/reddit.py`, process.env.CLIENT_ID, process.env.CLIENT_SECRET, process.env.USER_AGENT, subreddit];
+    // Run python with necessary arguments for reddit API.
+    const reddit = spawn('python', arguments);
 
-reddit.stdout.on('data', (data) => {
-  console.log(data.toString());
-});
+    reddit.stdout.on('data', (data) => {
+      // Converting the recieved string to array.
+      let str = data.toString();
+      str = str.replace(/['"\s\[\]]/g, '').split(',')
+      console.log(str);
+    });
 
-reddit.stderr.on('data', (data) => {
-  console.error(data.toString());
-});
+    reddit.stderr.on('data', (data) => {
+      console.error(data.toString());
+    });
+  });
+}
+
+const args = ['memes', 'pics']
+updateRedditPosts(args);
--- functions/reddit.py
+++ functions/reddit.py
@@ -1,14 +1,19 @@
 import praw
 import sys
 
-
+# Connecting to Reddit.
 reddit = praw.Reddit(
 	client_id=sys.argv[1],
 	client_secret=sys.argv[2],
 	user_agent=sys.argv[3],
 )
 
-posts = reddit.subreddit('memes').hot(limit=10)
+# Get the hot posts.
+posts = reddit.subreddit(sys.argv[4]).hot(limit=3)
+urls = [post.url for post in posts]
 
+# # Get the top posts.
+posts = reddit.subreddit(sys.argv[4]).top(limit=3)
 for post in posts:
-  print(post.url) 
+  urls.append(post.url)
+print(urls)
